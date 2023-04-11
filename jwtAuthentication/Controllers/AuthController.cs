e4654e5da6adeedce281c6569b667b6a544e3e16
Tharaka Madushan <mtharaka37@gmail.com>
2023-04-11 23:01:55 +0530

    commit -m  refresh Token

--- jwtAuthentication/Controllers/AuthController.cs
+++ jwtAuthentication/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace jwtAuthentication.Controllers
@@ -28,15 +29,6 @@ namespace jwtAuthentication.Controllers
         public ActionResult<string> GetMyName()
         {
             return Ok(_userService.GetMyName());
-
-            //var userName = User?.Identity?.Name;
-            //var roleClaims = User?.FindAll(ClaimTypes.Role);
-            //var roles = roleClaims?.Select(c => c.Value).ToList();
-            //var roles2 = User?.Claims
-            //    .Where(c => c.Type == ClaimTypes.Role)
-            //    .Select(c => c.Value)
-            //    .ToList();
-            //return Ok(new { userName, roles, roles2 });
         }
 
         [HttpPost("register")]
@@ -64,7 +56,59 @@ namespace jwtAuthentication.Controllers
 
             string token = CreateToken(users);
 
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken);
+
             return Ok(token);
+        }
+
+        [HttpPost("refresh-token")]
+
+        public  async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!users.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh token!");
+            }
+            else if (users.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token Expired!");
+            }
+
+            string token = CreateToken(users);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken);
+
+            return Ok(token);
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7)
+
+            };
+
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires,
+            };
+
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            users.RefreshToken = newRefreshToken.Token;
+            users.TokenCreated = newRefreshToken.Created;
+            users.TokenExpires = newRefreshToken.Expires;
         }
 
         private string CreateToken(Users users)
@@ -80,7 +124,7 @@ namespace jwtAuthentication.Controllers
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
             var token = new JwtSecurityToken(claims: claims,
-                expires: DateTime.Now.AddDays(1),
+                expires: DateTime.Now.AddHours(1),
                 signingCredentials: creds);
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
---
+++ jwtAuthentication/Models/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace jwtAuthentication.Models
+{
+    public class RefreshToken
+    {
+        public required string Token { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
