d8926dd2be79d4980a34f3ec698e8ad1030d5bea
Tharaka Madushan <mtharaka37@gmail.com>
2023-04-11 07:10:57 +0530

    commit -m Role based authorization with JSON Web tokens

--- jwtAuthentication/Controllers/AuthController.cs
+++ jwtAuthentication/Controllers/AuthController.cs
@@ -51,7 +51,11 @@ namespace jwtAuthentication.Controllers
         private string CreateToken(Users users)
         {
             List<Claim> claims = new List<Claim>
-            {new Claim(ClaimTypes.Name, users.Username)};
+            {
+                new Claim(ClaimTypes.Name, users.Username),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
 
